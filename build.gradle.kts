c4b33ceef939864a239c422fc68a997aa495ae21
Dirk-Jan Rutten <dirkjanrutten@gmail.com>
2020-04-10 20:33:30 +0200

    Updated kotlin version

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ project.version = versionNo
 
 
 plugins {
-    kotlin("jvm") version "1.3.21"
+    kotlin("jvm") version "1.3.71"
     `maven-publish`
     id("com.jfrog.bintray") version "1.8.1"
 
